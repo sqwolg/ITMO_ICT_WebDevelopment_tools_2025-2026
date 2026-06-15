44a82f66acea18a45682de4eca394b9cad028248
sqwolg <oleg_chernykh@list.ru>
2026-06-15 17:20:05 +0300

    Deployed 31b8318 with MkDocs version: 1.6.1

--- students/k3340/Chernykh_Oleg/Lr3/todo-app/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "Running migrations..."
-poetry run aerich upgrade
-
-echo "Starting application..."
-exec poetry run todo_app
--- students/k3340/Chernykh_Oleg/Lr3/todo-app/todo_app/services/__init__.py
+++
@@ -1,2 +0,0 @@
-from todo_app.services.users import UserService
-from todo_app.services.todos import TodoService
